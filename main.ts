7e7f4ee4ee66f08efbb592d0c5aa93a2b622cb1c
Sl1myz <147938695+Sl1myz@users.noreply.github.com>
2023-10-16 17:12:22 +0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,7 @@
 function fixMove (x: number, y: number) {
+    iBIT.Servo(ibitServo.SV1, 0)
+    iBIT.Servo(ibitServo.SV2, 0)
+    iBIT.MotorStop()
     ball_displacement_abs = Math.abs(ball_displacement_abs - 160)
     if (x > 180) {
         basic.showLeds(`
@@ -10,7 +13,7 @@ function fixMove (x: number, y: number) {
             `)
         iBIT.Turn(ibitTurn.Left, 30)
         basic.pause(ball_displacement_abs)
-        iBIT.Motor(ibitMotor.Forward, 10)
+        iBIT.Motor(ibitMotor.Forward, 5)
     } else if (x < 140) {
         basic.showLeds(`
             . . # . .
@@ -21,7 +24,7 @@ function fixMove (x: number, y: number) {
             `)
         iBIT.Turn(ibitTurn.Right, 30)
         basic.pause(ball_displacement_abs)
-        iBIT.Motor(ibitMotor.Forward, 10)
+        iBIT.Motor(ibitMotor.Forward, 5)
     } else {
         iBIT.Motor(ibitMotor.Forward, 30)
         basic.showLeds(`
@@ -34,20 +37,37 @@ function fixMove (x: number, y: number) {
     }
     ball_color = 0
 }
-function CollectBall () {
-    ball_displacement_y = huskylens.readeBox(ball_color, Content1.yCenter)
-    if (ball_displacement_y > -120) {
-        iBIT.Motor(ibitMotor.Forward, 50)
-        ball_displacement_y = huskylens.readeBox(ball_color, Content1.yCenter)
-    }
+input.onButtonPressed(Button.A, function () {
+    ShootBall()
+})
+input.onButtonPressed(Button.B, function () {
+    SwallowBall()
+})
+function ShootBall () {
+    state = 0
+    iBIT.Servo(ibitServo.SV1, 0)
+    iBIT.Servo(ibitServo.SV2, 0)
+    basic.pause(500)
+    iBIT.Servo(ibitServo.SV2, 90)
+    basic.pause(500)
+    iBIT.MotorStop()
+    iBIT.Servo(ibitServo.SV2, 0)
+}
+function SwallowBall () {
+    state = 1
+    iBIT.Servo(ibitServo.SV1, 120)
+    basic.pause(500)
+    iBIT.MotorStop()
+    ball_displacement_y = 0
 }
 let ball_displacement_abs = 0
 let ball_displacement_y = 0
 let ball_color = 0
+let state = 0
 huskylens.initI2c()
 huskylens.initMode(protocolAlgorithm.ALGORITHM_COLOR_RECOGNITION)
 huskylens.request()
-let state = 0
+state = 0
 ball_color = 0
 let ball_displacement_x = 0
 ball_displacement_y = 0
@@ -59,8 +79,13 @@ basic.forever(function () {
         ball_displacement_y = huskylens.readeBox(ball_color, Content1.yCenter)
     }
     if (state == 0) {
+        iBIT.Servo(ibitServo.SV1, 0)
         if (huskylens.isAppear(ball_color, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
-            fixMove(ball_displacement_x, ball_displacement_y)
+            if (ball_displacement_y < 200) {
+                fixMove(ball_displacement_x, ball_displacement_y)
+            } else if (ball_displacement_y < 300) {
+                SwallowBall()
+            }
         } else {
             basic.showLeds(`
                 . # # . .
@@ -72,5 +97,7 @@ basic.forever(function () {
             iBIT.MotorStop()
             ball_color = 0
         }
+    } else {
+        ShootBall()
     }
 })
