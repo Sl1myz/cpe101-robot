b545584db90f5340c44cc9ac46a76c547a15a584
Sl1myz <147938695+Sl1myz@users.noreply.github.com>
2023-10-16 10:55:50 +0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,17 @@
+function LookforQR () {
+	
+}
+function TurntoQR () {
+	
+}
+function ShootBall () {
+	
+}
+function CollectBall () {
+	
+}
+huskylens.initI2c()
+huskylens.initMode(protocolAlgorithm.ALGORITHM_COLOR_RECOGNITION)
 basic.forever(function () {
 	
 })
