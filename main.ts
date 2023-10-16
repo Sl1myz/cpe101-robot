2243720e7f0b47bd606c36b35907b8f2786678ed
Sl1myz <147938695+Sl1myz@users.noreply.github.com>
2023-10-16 14:57:55 +0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,6 @@
 function fixMove (x: number, y: number) {
-    iBIT.Servo(ibitServo.SV1, 0)
-    iBIT.Servo(ibitServo.SV2, 0)
-    if (x > 0) {
+    ball_displacement_abs = Math.abs(ball_displacement_abs - 160)
+    if (x > 180) {
         basic.showLeds(`
             . . # . .
             . # . . .
@@ -9,9 +8,10 @@ function fixMove (x: number, y: number) {
             . # . . .
             . . # . .
             `)
-        iBIT.Turn(ibitTurn.Left, 20)
-    } else if (x < 0) {
-        iBIT.Turn(ibitTurn.Right, 20)
+        iBIT.Turn(ibitTurn.Left, 30)
+        basic.pause(ball_displacement_abs)
+        iBIT.Motor(ibitMotor.Forward, 10)
+    } else if (x < 140) {
         basic.showLeds(`
             . . # . .
             . . . # .
@@ -19,20 +19,20 @@ function fixMove (x: number, y: number) {
             . . . # .
             . . # . .
             `)
+        iBIT.Turn(ibitTurn.Right, 30)
+        basic.pause(ball_displacement_abs)
+        iBIT.Motor(ibitMotor.Forward, 10)
     } else {
-        iBIT.Motor(ibitMotor.Forward, 20)
+        iBIT.Motor(ibitMotor.Forward, 30)
         basic.showLeds(`
             . . # . .
-            . # # # .
-            # . # . #
             . . # . .
+            # . # . #
+            . # # # .
             . . # . .
             `)
-        if (y < -90) {
-            iBIT.MotorStop()
-            CollectBall()
-        }
     }
+    ball_color = 0
 }
 function CollectBall () {
     ball_displacement_y = huskylens.readeBox(ball_color, Content1.yCenter)
@@ -41,6 +41,7 @@ function CollectBall () {
         ball_displacement_y = huskylens.readeBox(ball_color, Content1.yCenter)
     }
 }
+let ball_displacement_abs = 0
 let ball_displacement_y = 0
 let ball_color = 0
 huskylens.initI2c()
@@ -48,16 +49,28 @@ huskylens.initMode(protocolAlgorithm.ALGORITHM_COLOR_RECOGNITION)
 huskylens.request()
 let state = 0
 ball_color = 0
-let turn_num = 0
 let ball_displacement_x = 0
 ball_displacement_y = 0
 basic.forever(function () {
     if (ball_color == 0) {
+        huskylens.request()
         ball_color = huskylens.readBox_s(Content3.ID)
+        ball_displacement_x = huskylens.readeBox(ball_color, Content1.xCenter)
+        ball_displacement_y = huskylens.readeBox(ball_color, Content1.yCenter)
     }
-    ball_displacement_x = huskylens.readeBox(ball_color, Content1.xCenter)
-    ball_displacement_y = huskylens.readeBox(ball_color, Content1.yCenter)
     if (state == 0) {
-        fixMove(ball_displacement_x, ball_displacement_y)
+        if (huskylens.isAppear(ball_color, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
+            fixMove(ball_displacement_x, ball_displacement_y)
+        } else {
+            basic.showLeds(`
+                . # # . .
+                . # # . .
+                # # # # #
+                # # # # #
+                . # # # .
+                `)
+            iBIT.MotorStop()
+            ball_color = 0
+        }
     }
 })
